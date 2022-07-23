1cd3f3d3b7735e5c4e362cd9fc7db677790748aa
qq449245884 <449245884@qq.com>
2022-07-23 15:18:27 +0800

    feat: 完成基本版本

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <User :name="userInfo.name" :job="userInfo.job" :age="userInfo.age" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { reactive } from 'vue'
+import User from './components/User.vue'
+
+const userInfo = reactive({
+  name: '王大冶',
+  job: '前端开发',
+  age: 16,
+})
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>姓名：{{ name }}</p>
+    <p>职业：{{ job }}</p>
+    <p>年龄：{{ age }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: String,
+  job: String,
+  age: Number,
+})
+
+const count = ref(0)
+</script>
