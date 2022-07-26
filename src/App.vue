e660b86da4e69d44217630584b39cef3901ac05e
wangzhiyi <35094245+qq449245884@users.noreply.github.com>
2022-07-26 10:10:57 +0800

    feat: 创建04

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,6 @@
 
 <script setup>
 import { reactive } from 'vue'
-import User from './components/User.vue'
 
-const userInfo = reactive({
-  name: '王大冶',
-  job: '前端开发',
-  age: 16,
-})
-
-const userEventHandler = {
-  updateUserName(userName) {
-    alert(userName)
-  },
-  updateUserAge(userAge) {
-    alert(userAge)
-  },
-}
+// https://v3.cn.vuejs.org/api/refs-api.html#toref
 </script>
