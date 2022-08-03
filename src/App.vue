1dd470236d7342bee8afc3aa3053cc1cf0046e9f
qq449245884 <449245884@qq.com>
2022-08-04 07:43:51 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <div>
     <ul>
-      <li v-for="(value, key, index) in {
-        name: '王大冶',
-        age: 26,
-        job: '前端开发'
-      }">
-       #{{ index + 1 }} {{ key }}: {{ value }}
+      <li v-for="c in 'hello'">
+        {{ c }}
       </li>
     </ul>
   </div>
 </template>
-
-<script setup>
-import { reactive } from 'vue'
-
-const userInfo = reactive({
-  id: '001',
-  name: '王大冶',
-  age: 16,
-})
-</script>
-
-<style>
-ul,
-li {
-  margin: 0;
-  padding: 0;
-  text-align: left;
-}
-</style>
