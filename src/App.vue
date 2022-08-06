26cf8247781036187d0a98abae1bfcce83e77bd4
qq449245884 <449245884@qq.com>
2022-08-06 14:38:07 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="c in 'hello'">
-        {{ c }}
-      </li>
-    </ul>
-  </div>
+  <Button>
+    点击我<strong>领取</strong> <img />
+  </Button>
 </template>
+
+<script setup>
+import Button from './components/Button.vue'
+</script>
