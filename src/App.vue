746383b8e7f57245e180c1d238727566f0e78fae
qq449245884 <449245884@qq.com>
2022-08-14 23:57:40 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <Toggle title="Toggled 内容" :hidden="hidden" @click="hidden = !hidden">
+  <Widget title="Toggled 内容" :hidden="hidden" @click="hidden = !hidden">
     通过点击来切换显隐藏内容
-  </Toggle>
+  </Widget>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Toggle from './components/Toggle.vue'
-
-const hidden = ref(false)
+import Widget from './components/Widget.vue'
 </script>
---
+++ src/components/Widget.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { getCurrentInstance } from 'vue'
+
+const instanc = getCurrentInstance()
+
+console.log(instanc)
+// export default {
+//   name: 'Widget',
+//   columns: 3,
+//   created() {
+//     console.log(`Using ${this.$options.columns} columns`)
+//   },
+// }
+</script>
