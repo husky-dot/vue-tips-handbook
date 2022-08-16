fdd469324ef5e5075ecdeeda91b2f04b9e990d2d
qq449245884 <449245884@qq.com>
2022-08-16 22:52:36 +0800

    feat: 完成

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
-</template>
+  <div>
+    <h2>{{ title }}</h2>
+    <div class="navigation">
+      <!-- ... -->
+    </div>
 
+    <div v-for="item in list">
+      <h2 class="item-title">
+        {{ item.title }}
+      </h2>
+      <p class="item-description">
+        {{ item.description }}
+      </p>
+    </div>
+
+    <footer>
+      <!-- ... -->
+    </footer>
+  </div>
+</template>
 <script setup>
 import { getCurrentInstance } from 'vue'
 
