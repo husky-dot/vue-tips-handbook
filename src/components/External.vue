0903673b1922a62c69d1e487f0597259da093a54
qq449245884 <449245884@qq.com>
2022-08-20 14:43:39 +0800

    feat: 完成

--- src/components/External.vue
+++ src/components/External.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    y: {{ reactiveVariable.x }}
-    x: {{ reactiveVariable.y }}
-  </div>
+  <div>y: {{ reactiveVariable.x }} x: {{ reactiveVariable.y }}</div>
 </template>
 
-<script setup>
-import { reactive } from 'vue'
-import { getValue } from '../external'
-
-const reactiveVariable = reactive(getValue())
+<script>
+export default {
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`
+    },
+  },
+  watch: {
+    fullName() {
+      this.fetchUserInformation() // Side effect
+    },
+  },
+}
 </script>
