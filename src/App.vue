3389d6f7339e887b619cc06a872202fb449084bb
qq449245884 <449245884@qq.com>
2022-08-21 21:16:56 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <VPre />
+  <SVGComponent :color="isGreen ? '#00CDA3' : '#032A5E'" />
 </template>
 
-<script setup>
-import VPre from './components/VPre.vue'
+<script>
+import { ref } from 'vue'
+import SVGComponent from './components/SVGComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    SVGComponent,
+  },
+  setup() {
+    const isGreen = ref(true)
+
+    // Toggle the color back and forth
+    setInterval(() => {
+      isGreen.value = !isGreen.value
+    }, 500)
+
+    return { isGreen }
+  },
+}
 </script>
---
+++ src/components/LinkComponent copy.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="50" :fill="color" />
+  </svg>
+</template>
+<script>
+export default {
+  name: 'SVGComponent',
+  props: {
+    color: String,
+  },
+}
+</script>
