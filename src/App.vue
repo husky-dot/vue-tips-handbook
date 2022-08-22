4eac94159815f68a71032c582dbe6d3b430e761e
qq449245884 <449245884@qq.com>
2022-08-22 23:57:12 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <SVGComponent :color="isGreen ? '#00CDA3' : '#032A5E'" />
+  <div>
+    <MenuItem v-for="item in menuItems" @click="item.action">
+      {{ item.text }}
+    </MenuItem>
+  </div>
 </template>
 
-<script>
-import { ref } from 'vue'
-import SVGComponent from './components/SVGComponent.vue'
-export default {
-  name: 'App',
-  components: {
-    SVGComponent,
-  },
-  setup() {
-    const isGreen = ref(true)
+<script setup>
+import MenuItem from './components/MenuItem.vue'
 
-    // Toggle the color back and forth
-    setInterval(() => {
-      isGreen.value = !isGreen.value
-    }, 500)
-
-    return { isGreen }
-  },
+const onCreate = () => {
+  // todo
 }
+const onDelete = () => {
+  // todo
+}
+const onEdit = () => {
+  // todo
+}
+const onSearch = () => {
+  // todo
+}
+
+const menuItems = [
+  {
+    text: '新增',
+    action: onCreate,
+  },
+  {
+    text: '删除',
+    action: onDelete,
+  },
+  {
+    text: '修改',
+    action: onEdit,
+  },
+  {
+    text: '查询',
+    action: onSearch,
+  },
+]
 </script>
