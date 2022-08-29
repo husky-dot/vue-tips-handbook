98280258eb22221db6af800b2099dabdb3175270
qq449245884 <449245884@qq.com>
2022-08-29 23:32:41 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <User />
-  </div>
+  <button @click="triggerError = !triggerError">触发 Error</button>
+  <span v-if="triggerError">
+    <!-- 这里将会引发一个错误，因为试图执行一个不存在的方法 -->
+    {{ hello.oops() }}
+  </span>
+  <span v-else>
+    <!-- 这里会产生一个警告，因为`hello`不存在。 -->
+    {{ hello }}
+  </span>
 </template>
 
 <script setup>
-import User from './components/User.vue'
-
+import { ref } from 'vue'
+const triggerError = ref(false)
 </script>
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-// import { createApp } from 'vue/dist/vue.esm-bundler.js'
-import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.config.globalProperties.$myGlobal = '全局属性'
+app.config.errorHandler = (err) => {
+  alert(err)
+}
+
+app.config.warnHandler = (warn) => {
+  console.warn(`[CUSTOM WARNING] ${warn}`)
+}
 
 app.mount('#app')
