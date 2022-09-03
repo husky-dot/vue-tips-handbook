9bc459a62325fa45502cc0d48b9bee23671d7367
qq449245884 <449245884@qq.com>
2022-09-03 23:04:03 +0800

    feat: 完成

---
+++ src/components/CheckSlot.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer v-if="showFooter">
+    <h3>来自底层的仰望</h3>
+    <slot name="footer" />
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showFooter: false,
+    }
+  },
+  created() {
+    this.setShowSlots()
+  },
+  beforeUpdate() {
+    this.setShowSlots()
+  },
+  methods: {
+    setShowSlots() {
+      this.showFooter = this.$slots.footer?.[0]
+    },
+  },
+}
+</script>
