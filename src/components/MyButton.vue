037bb522510f00fe8ee1f9ad636c8e1bea24eda2
qq449245884 <449245884@qq.com>
2022-09-05 23:08:01 +0800

    feat: 完成

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,11 +1,20 @@
-<template>
-  <div>
-    <button>click me</button>
-  </div>
-</template>
+<script setup>
+import { computed, ref } from 'vue'
 
-<style>
-.red {
-  color: red;
-}
-</style>
+const count = ref(0)
+
+const plusOne = computed(() => count.value + 1, {
+  onTrack(e) {
+    // 当 count.value 作为依赖被追踪时触发
+    debugger
+  },
+  onTrigger(e) {
+    // 当 count.value 被修改时触发
+    debugger
+  },
+})
+// 访问 plusOne，应该触发 onTrack
+console.log(plusOne.value)
+// 修改 count.value，应该触发 onTrigger
+count.value++
+</script>
