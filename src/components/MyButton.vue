0235864ade1fcbaebdf1125d8f0c74fa46a5367a
qq449245884 <449245884@qq.com>
2022-09-07 00:17:07 +0800

    feat:  完成

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,20 +1,15 @@
+<template>
+  <input v-model.lazy="message" />
+</template>
+
 <script setup>
-import { computed, ref } from 'vue'
-
-const count = ref(0)
-
-const plusOne = computed(() => count.value + 1, {
-  onTrack(e) {
-    // 当 count.value 作为依赖被追踪时触发
-    debugger
-  },
-  onTrigger(e) {
-    // 当 count.value 被修改时触发
-    debugger
-  },
+import { watch, ref } from 'vue'
+const message = ref('')
+const saveMessage = () => {
+  console.log('做一些保存数据的操作')
+}
+watch(message, (newMessage) => {
+  // 这里只有在触发 change 事件的时候才会调用
+  saveMessage(newMessage)
 })
-// 访问 plusOne，应该触发 onTrack
-console.log(plusOne.value)
-// 修改 count.value，应该触发 onTrigger
-count.value++
 </script>
