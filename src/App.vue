d040a5b8e2ecf77f0aa17936577e1974c1aa72a5
qq449245884 <449245884@qq.com>
2022-10-22 22:32:59 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <my-button
-    data-cy="button"
-    class="dark-button"
-    id="the-button"
-    @click="doSomething()"
-    aria-label="A nice button"
-    disabled="true"
-    undefinedProperty="ops"
-  />
+  <div id="app">
+    <SimpleEvent
+      id="myId"
+      class="myClass"
+      data-cy="cypress-testing"
+      @blur="onBlur"
+      title="测试$attrs"
+      description="没有声明 props"
+      @custom="onCustom"
+    />
+  </div>
 </template>
 
 <script setup>
-import MyButton from './components/Button.vue'
+import SimpleEvent from './components/Button.vue'
 
+const onBlur = () => {
+  // todo
+}
+const onCustom = () => {
+  // todo
+}
 </script>
