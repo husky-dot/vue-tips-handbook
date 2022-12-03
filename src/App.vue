dee6d04f4e947e723c56dcd436c6f6672ac6ab5a
qq449245884 <449245884@qq.com>
2022-12-03 14:30:54 +0800

    feat: 完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    <Component1 />
-    <Component2 />
-  </div>
+  <h1>{ title }</h1>
 </template>
 
 <script setup>
-import Component1 from './components/Component1.vue'
-import Component2 from './components/Component2.vue'
-import { ref } from 'vue'
-const input = ref('')
+const title = 'Hello'
 </script>
